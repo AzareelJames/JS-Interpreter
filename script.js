646c73a247af1d88fdcd2b60b05135c70698fe2d
AzareelJames <azareeljameslbasalo@gmail.com>
2025-12-13 11:49:21 +0800

    Refactor command execution in script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const exec = e => {
 
     (() => {
         try{
-            write(new Function(`return {"js": ${cmd.replaceAll("`", "\\`")}}["js"]()`)(), "gray");
+            write(new Function(`return {"js": () => {${cmd.replaceAll("`", "\\`")}["js"]()}}`)(), "gray");
         } catch(E){
             console.error(`Uncaught ${E}`);
         }
@@ -58,4 +58,5 @@ input.addEventListener("keydown", exec);
 if(URLParam){
     exec({"key": "Enter"});
 }
+
 
